d39afa74417197acfa8be917901b6744028072cb
grluk <48202919+grluk@users.noreply.github.com>
2021-11-03 12:48:40 +0000

    Update ConsoleSetup.sh

--- ConsoleSetup.sh
+++ ConsoleSetup.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-#Add Home dir local bin folder. Anything with execute permissions in this folder will be available in PATH.
+# Add Home dir local bin folder. Anything with execute permissions in this folder will be available in PATH.
+echo "#Changes below here added manually" >> ~/.bashrc
 echo "-- Adding local bin path.--"
-echo "PATH=$PATH:~/bin" >> ~/.bashrc
-if [ -d "~/bin" ]; then
+if [ -d "$HOME/bin" ]; then
   echo "Directory ~/bin already exists.. skipping."
 else
   mkdir ~/bin
 fi
+echo "PATH='$PATH:~/bin'" >> ~/.bashrc
 
 echo "-- Adding time stamps to history output --"
-#Add Timestamps for history file.
+# Add Timestamps for history file.
 echo "export HISTTIMEFORMAT='%F %T '" >> ~/.bashrc
 
-#Alias
-#Add nifty nmap script searcher.
+# Alias
+# Add nifty nmap script searcher.
 echo "-- Adding Alias --"
 echo "alias nmapls='ls /usr/share/nmap/scripts | grep $1'" >> ~/.bashrc 
-echo "alias gh='history | grep $1' >> ~/.bashrc #This one from Rob Jervis!
+echo "alias gh='history | grep $1'" >> ~/.bashrc #This one from Rob Jervis!
 
 echo "Done, you may need to restart the console session"
