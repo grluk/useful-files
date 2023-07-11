a6aa89e8e32fe3f3b43c6f4a84625547cd1ca158
grluk <48202919+grluk@users.noreply.github.com>
2023-07-11 10:29:51 +0100

    Update essentials.sh
    Added git fetch repos, enum4linux, username stats, kerbrute

--- essentials.sh
+++ essentials.sh
@@ -2,3 +2,7 @@
 
 sudo apt install sshfs sshuttle flameshot
 
+cd /opt
+git clone https://github.com/cddmp/enum4linux-ng.git
+git clone https://github.com/insidetrust/statistically-likely-usernames.git
+wget -c https://github.com/ropnop/kerbrute/releases/download/v1.0.3/kerbrute_linux_amd64
